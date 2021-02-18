f90b0d1462572e2e7ca917c547a54383b0d4a62f
Jahn08 <jahn_08@mail.ru>
2021-02-18 15:36:51 +0300

    Sorting items in dropdowns alphabetically.

--- lib/src/dialogs/word_selector_dialog.dart
+++ lib/src/dialogs/word_selector_dialog.dart
@@ -4,11 +4,15 @@ import 'single_selector_dialog.dart';
 import '../models/word.dart';
 
 class WordSelectorDialog extends SingleSelectorDialog<Word> {
-    WordSelectorDialog(BuildContext context):
+
+	final BuildContext context;
+
+    WordSelectorDialog(this.context):
         super(context, AppLocalizations.of(context).wordSelectorDialogTitle);
    
     @override
-    Widget getItemSubtitle(Word item) => new Text(item.partOfSpeech);
+    Widget getItemSubtitle(Word item) => 
+		new Text(item.partOfSpeech.present(AppLocalizations.of(context)));
   
     @override
     String getItemTitle(Word item) => item.translations.join('; ');
