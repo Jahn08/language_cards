4a3d5613aa306392c77f8481c34be89999b34926
Jahn08 <jahn_08@mail.ru>
2021-02-23 21:54:46 +0300

    Storing only a facebook user id as a setting rather than its whole user.

--- lib/src/models/app_params.dart
+++ lib/src/models/app_params.dart
@@ -17,31 +17,32 @@ class DictionaryParams {
 }
 
 class ContactsParams {
-    static const String _fbLinkPropName = 'facebook';
+    static const String _fbLinkPropName = 'facebook_user_id';
     static const String _emailPropName = 'email';
 	static const String _appStoreIdPropName = 'app_store_id';
 
-    final String fbLink;
+    final String fbUserId;
 
     final String email;
 
     final String appStoreId;
 
-    ContactsParams({ this.fbLink, this.email, this.appStoreId });
+    ContactsParams({ this.fbUserId, this.email, this.appStoreId });
 
     ContactsParams.fromJson(Map<String, dynamic> json): 
-        fbLink = json[_fbLinkPropName],
+        fbUserId = json[_fbLinkPropName],
         appStoreId = json[_appStoreIdPropName],
         email = json[_emailPropName];
 
     ContactsParams merge(ContactsParams params) =>
 		new ContactsParams(
-			fbLink: getValueOrDefault(params?.fbLink, fbLink),
+			fbUserId: getValueOrDefault(params?.fbUserId, fbUserId),
 			email: getValueOrDefault(params?.email, email),
 			appStoreId: getValueOrDefault(params?.appStoreId, appStoreId)
 		);
 
-    Map<String, dynamic> toJson() => { _fbLinkPropName: fbLink };
+    Map<String, dynamic> toJson() => 
+		{ _fbLinkPropName: fbUserId, _emailPropName: email, _appStoreIdPropName: appStoreId };
 }
 
 class AppParams {
@@ -55,13 +56,14 @@ class AppParams {
     AppParams({ this.dictionary, this.contacts});
 
     AppParams.fromJson(Map<String, dynamic> json): 
-        dictionary = new DictionaryParams.fromJson(json[_dictionaryPropName]),
-		contacts = new ContactsParams.fromJson(json[_contactsPropName]);
+        dictionary = new DictionaryParams.fromJson(json[_dictionaryPropName] ?? {}),
+		contacts = new ContactsParams.fromJson(json[_contactsPropName] ?? {});
 
     AppParams merge(AppParams params) {
-        return new AppParams(dictionary: params?.dictionary ?? dictionary,
-			contacts: params?.contacts ?? contacts);
+        return new AppParams(dictionary: dictionary?.merge(params?.dictionary),
+			contacts: contacts?.merge(params?.contacts));
     }
 
-    Map<String, dynamic> toJson() => {_dictionaryPropName: dictionary };
+    Map<String, dynamic> toJson() => 
+		{ _dictionaryPropName: dictionary, _contactsPropName: contacts };
 }
