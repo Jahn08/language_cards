921ba607e3efdd008791a4b2db8d29ecc26afc89
Jahn08 <jahn_08@mail.ru>
2021-03-14 19:07:44 +0300

    Immediate adaptations to to the latest packages.

--- lib/src/data/word_dictionary.dart
+++ lib/src/data/word_dictionary.dart
@@ -5,24 +5,34 @@ import '../models/article.dart';
 import '../models/language.dart';
 
 class WordDictionary {
-    final String _uri;
+    final Uri _uri;
     final Client _client;
 
     WordDictionary(String apiKey, { @required Language from, Language to, Client client }): 
         _uri = _buildFullUri(apiKey, from, to),
         _client = client ?? new Client();
 
-    static String _buildFullUri(String key, Language from, [Language to]) {
+    static Uri _buildFullUri(String key, Language from, [Language to]) {
         final _from = _representLanguage(from);
         final _to = _representLanguage(to ?? from);
-        return 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=$key&lang=$_from-$_to';
+        return new Uri(
+			host: 'dictionary.yandex.net', 
+			pathSegments: ['api', 'v1', 'dicservice.json', 'lookup'],
+			queryParameters: {
+				'key': key,
+				'lang': '$_from-$_to'
+			}
+		);
     }
 
     static String _representLanguage(Language lang) =>
         lang == Language.russian ? 'ru': 'en';
 
     Future<Article> lookUp(String word) async {
-        final response = await _client.post(_uri + '&text=$word');
+		final params = new Map<String, dynamic>.of(_uri.queryParameters);
+		params['text'] = word;
+
+        final response = await _client.post(Uri.https(_uri.authority, _uri.path, params));
         return new Article.fromJson(jsonDecode(response.body));
     }
 
